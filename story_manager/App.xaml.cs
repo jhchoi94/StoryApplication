0b8d18278b1371d393d538dc9026bd869477a72e
jhchoi <jhchoi@DESKTOP-BCCL345>
2021-11-18 11:30:36 +0900

    초기 커밋.

---
+++ story_manager/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using story_manager.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace story_manager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ story_manager/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace story_manager.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
